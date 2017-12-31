c9968b431126a1d01eeaf8b14b11a532ec42f1b2
Kyle Helmick <helmick.kyle@gmail.com>
2017-12-30 20:11:25 -0700

    Committed before I commented 1.5 and realized that I error check something more than I needed to

--- Chapter1/Question5.cpp
+++ Chapter1/Question5.cpp
@@ -33,8 +33,12 @@ bool oneAway(string str1, string str2) {
     return false;
   }
 
+  // This boolean will keep track of whether we've identified a difference
+  //    or not
   bool difference_found = false;
 
+  // Basically if the strings are the same size we just need to make sure there's
+  //    only one difference between the two strings
   if(diff == 0) {
     for(int i = 0; i < str1.length(); i++) {
       if(str1[i] != str2[i] && !difference_found) {
@@ -45,29 +49,44 @@ bool oneAway(string str1, string str2) {
     }
     return true;
   } else {
+    // These modifiers will help keep track of the current indices for
+    //    str1 and str2
     int modifier1 = 0;
     int modifier2 = 0;
     
     int shortlen = 0;
 
+    // We also want to use the shorter string as our "base" string so we
+    //    don't have to worry about any segfaults or going out of bounds
+    //    of arrays
     if(str1.length() < str2.length()){
       shortlen = str1.length();
     } else {
       shortlen = str2.length();
     }
 
+    // This boolean will keep track of whether we've identified a difference
+    //    or not
     bool difference_found = false;
 
     for(int i = 0; i < shortlen; i++) {
+      // First if there's a difference and we haven't identified a difference yet
       if(str1[i+modifier1] != str2[i+modifier2] && !difference_found) {
+        // Make sure we note that we just found a difference
         difference_found = true;
+        // If string 1 is "ahead" of string 2 then we want to catch string 2 up
         if(str1[i] == str2[i+1]) {
           modifier2 += 1;
+        // If string 2 is "ahead" of string 1 then we want to catch string 1 up
         } else if (str1[i+1] == str2[i]) {
           modifier1 += 1;
+        // Otherwise we've found different characters where the string aligns afterwards
+        //    which is similar to our first case (same length) and creates a problem because
+        //    of the difference of string lengths
         } else {
           return false;
         }
+      // If we find another difference the strings are not one apart
       } else if((str1[i+modifier1] != str2[i+modifier2]) && difference_found) {
         return false;
       }
@@ -75,8 +94,5 @@ bool oneAway(string str1, string str2) {
 
   }
 
-  if(difference_found) {
-    return false;
-  }
   return true;
 }
